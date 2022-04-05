88157854cf4e938991ee6dae7e956a13828f457b
maravoly <82736249+maravoly@users.noreply.github.com>
2022-04-05 11:25:59 -0400

    Add files via upload

---
+++ ejercicio_1.rb
@@ -0,0 +1,15 @@
+#Curso Emprendimiento Digital con Tecnologías Web
+
+#MÓDULO 6: INTRODUCCIÓN A LA PROGRAMACIÓN CON RUBY
+
+#Realiza un programa que recibe 2 números por pantalla e imprime el resultado de su suma en el siguiente formato:
+#“La suma de los números 2 y 3 es igual a 5”
+
+puts "Ingrese primer número"
+num1 = gets.to_i
+puts "Ingrese segundo número"
+num2 = gets.to_i
+suma = num1 + num2
+print "La suma de los números" num1 "y" num2 "es igual a" suma
+
+puts "La suma de los numeros #{num1} y #{num2} es igual a #{suma}"
